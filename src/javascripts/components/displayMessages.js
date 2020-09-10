35e4d42c65aad0bc4b52e2e8e45838c624ce5982
bandstrar <rob.bandstra@gmail.com>
2020-09-09 21:18:29 -0500

    added delete buttons to the messages

--- src/javascripts/components/displayMessages.js
+++ src/javascripts/components/displayMessages.js
@@ -1,17 +1,20 @@
 import Data from '../helpers/data/messageData';
 import Image from '../../assets/images/woodstock.gif';
+import Delete from './removeMessage';
 
 const displayMessage = (obj) => {
   const domString = `
-  <li class="media mb-3">
+  <li class="media mb-3" id="message${obj.id}">
     <img class="mr-3 rounded-circle" src="${Image}" alt="${obj.user}">
     <div class="media-body">
       <h5 class="mt-0 mb-1">${obj.user}</h5>
       ${obj.message}
+      <button type="button" class="close" id="delete${obj.id}"><span>&times;</span></button>
     </div>
   </li>`;
 
   $('#messages-list').append(domString);
+  $(`#delete${obj.id}`).on('click', Delete.removeMessage);
 };
 
 const printMessages = () => {
