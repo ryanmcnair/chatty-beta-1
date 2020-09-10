3057de8e08b38d472141d551a69800f7a7b08ca6
Michael Pfohl <rmpfohl@gmail.com>
2020-09-10 18:24:34 -0500

    refactor add message and add ability to press enter

--- src/javascripts/components/addMessage.js
+++ src/javascripts/components/addMessage.js
@@ -1,20 +1,30 @@
 import Data from '../helpers/data/messageData';
 import Print from './displayMessages';
 
+const messageAction = () => {
+  if ($('#message-input').val() === '') {
+    $('#error').html('<p>Please enter a message</p>');
+  } else {
+    const newMessage = {
+      id: 'message6',
+      user: 'Woodstock',
+      message: $('#message-input').val(),
+    };
+    Data.getMessages().push(newMessage);
+    Print.printMessages();
+    $('#message-input').val('');
+    $('#error').html('');
+  }
+};
+
 const addMessage = () => {
   $('#send').on('click', () => {
-    if ($('#message-input').val() === '') {
-      $('#error').html('<p>Please enter a message</p>');
-    } else {
-      const newMessage = {
-        id: 'message6',
-        user: 'Woodstock',
-        message: $('#message-input').val(),
-      };
-      Data.getMessages().push(newMessage);
-      Print.printMessages();
-      $('#message-input').val('');
-      $('#error').html('');
+    messageAction();
+  });
+  $('#message-input').on('keypress', (e) => {
+    if (e.which === 13) {
+      e.preventDefault();
+      messageAction();
     }
   });
 };
