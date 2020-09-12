730e6bfdf5860dc8bbe24c55efa81f80b0ed31be
Michael Pfohl <rmpfohl@gmail.com>
2020-09-11 19:17:08 -0500

    add clean timestamp to messages

--- src/javascripts/components/displayMessages.js
+++ src/javascripts/components/displayMessages.js
@@ -8,7 +8,8 @@ const displayMessage = (obj) => {
     <img class="mr-3 rounded-circle" src="${Image}" alt="${obj.user}">
     <div class="media-body">
       <h5 class="mt-0 mb-1">${obj.user}</h5>
-      ${obj.message}
+      <p class="message--content">${obj.message}</p>
+      <p class="timestamp">${obj.timestamp}</p>
       <button type="button" class="close" id="delete${obj.id}"><span>&times;</span></button>
     </div>
   </li>`;
--- src/javascripts/helpers/data/messageData.js
+++ src/javascripts/helpers/data/messageData.js
@@ -2,27 +2,32 @@ const messageArray = [
   {
     id: 'message1',
     user: 'Woodstock',
-    message: '*Chittering noises*'
-  },
-  {
-    id: 'message2',
-    user: 'Snoopy',
-    message: 'LOL'
-  },
-  {
-    id: 'message3',
-    user: 'Charlie Brown',
-    message: 'Good grief...'
+    message: '*Chittering noises*',
+    timestamp: 'April 4, 1967, 7:03 pm'
   },
   {
     id: 'message4',
     user: 'Lucy',
-    message: 'Hey Schroeder, play me that song, you know the one by Vince Guaraldi'
+    message: 'Hey Schroeder, play me that song, you know the one by Vince Guaraldi',
+    timestamp: 'March 3, 1952, 10:11 pm'
   },
   {
     id: 'message5',
     user: 'Schroeder',
-    message: 'Christmastime Is Here? But it\'s September!'
+    message: 'Christmastime Is Here? But it\'s September!',
+    timestamp: 'May 30, 1951, 8:48 pm'
+  },
+  {
+    id: 'message2',
+    user: 'Snoopy',
+    message: 'LOL',
+    timestamp: 'October 4, 1950, 12:02 am'
+  },
+  {
+    id: 'message3',
+    user: 'Charlie Brown',
+    message: 'Good grief...',
+    timestamp: 'October 2, 1950, 6:15 am'
   }
 ];
 
