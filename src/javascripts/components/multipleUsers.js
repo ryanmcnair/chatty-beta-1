8d03f6022ea97c2614a0573e9d6b34e8cbabb918
Ryan McNair <ryan.mcnair@gmail.com>
2020-09-11 19:27:42 -0500

    made some form input changes

--- src/javascripts/components/multipleUsers.js
+++ src/javascripts/components/multipleUsers.js
@@ -3,7 +3,7 @@ import Users from '../helpers/data/messageData';
 const userSelection = () => {
   Users.getMessages().forEach((item) => {
     $('#settings-container').append(`
-      <div class="form-check">
+      <div class="form-check" name ="radio-selector">
         <input class="form-check-input" type="radio" name="character" id="${item.id}-radio" value="${item.user}" checked>
         <img class="mr-3 rounded-circle" src="${item.image}"/></br>
         <label class="form-check-label" for="exampleRadios1">
@@ -12,5 +12,18 @@ const userSelection = () => {
       `);
   });
 };
+
+// const userValue = [];
+
+// const form = document.forms.radio-selector;
+// const radios = form.elements.characters;
+
+// userValue.innerText = radios.value;
+
+// radios.forEach(radio => {
+//     radio.addEventListener('change', () => {
+//       currentValue.innerText = radios.value;
+//     });
+//   });
 
 export default { userSelection };
